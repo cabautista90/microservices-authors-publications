82b9d05d8597d6b41cf4c3ca05e1b53fddcafbaa
cabautista90 <cabautista@espe.edu.ec>
2026-02-01 21:50:37 -0500

    cambio de puerto salida API
    SE CAMBIA PUERTO PAR PRUEBAS DE ENDPOINTS

--- authors-service/src/main/java/com/editorial/authors/authors_service/authors/api/controller/AuthorController.java
+++ authors-service/src/main/java/com/editorial/authors/authors_service/authors/api/controller/AuthorController.java
@@ -1,14 +1,19 @@
-package com.editorial.authors.api.controller;
+package com.editorial.authors.authors_service.authors.api.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.editorial.authors.api.request.CreateAuthorRequest;
 import com.editorial.authors.api.response.AuthorResponse;
 import com.editorial.authors.application.service.AuthorService;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/authors")
